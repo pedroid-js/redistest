deb191b73a58f0d50af3cc0a2a3b0c289fb51f41
Pedro Fernandez Caffarena <58910797+pedroid-js@users.noreply.github.com>
2021-03-04 13:13:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,21 @@ const { doScanAsync, doScan, doInsert } = require('./redis/redisFunctions.js')
 /**
  * static void main args[]
  */
-async function main() {
+
+async function scan(pageSize = '25') {
   try {
-    const { found, count, success } = await doScanAsync("*bfed*")
+    const { found, count, elapsed_time, success } = await doScanAsync("*DAJcUm*", 1, pageSize)
     if (success) {
       return {
         found,
-        count
+        count,
+        elapsed_time
       }
     } else {
       return {
-        "msg": `found ${count} keys for this pattern`
+        "msg": `found ${count} keys for this pattern`,
+        count,
+        elapsed_time
       }
     }
   } catch(e) {
@@ -23,12 +27,21 @@ async function main() {
   }
 }
 
+async function main() {
+  const scanc25 = await scan()
+  const scanc1000 = await scan('1000')
+  const scanc10000 = await scan('10000')
+
+  return {
+    scanc25,
+    scanc1000,
+    scanc10000
+  }
+}
 
 main().then((data) => {
   console.log(data)
 })
-
-
 
 //console.log(doScan("*-*"))
 //doScan("*DClmedSJQcqZ*")
