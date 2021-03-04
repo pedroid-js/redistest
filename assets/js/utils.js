032c58c87e50dcf086511ceeffd619b6dffc74ac
Pedro Fernandez Caffarena <58910797+pedroid-js@users.noreply.github.com>
2021-03-04 12:56:52 +0100

    Update utils.js
    added function to meassure time

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -8,6 +8,20 @@ function gchar(length) {
 	return result;
 }
 
+/**
+ * 
+ * @param {process.hrtime()} start 
+ */
+function elapsed_time (start, note) {
+	let e = null
+	const precision = 3
+	const elapsed = process.hrtime(start)[1]/1000000
+	e = process.hrtime(start)[0] + " s, " + elapsed.toFixed(precision) + " ms - " + note
+	start = process.hrtime()
+	return e
+}
+
 module.exports = {
-  gchar
+	elapsed_time,
+  	gchar
 }
